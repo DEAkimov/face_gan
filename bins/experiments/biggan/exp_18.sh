159e02947d7e3b0b662116dccfb8a1e072a477b6
DEAkimov <deakimov@edu.hse.ru>
2019-02-02 10:58:32 +0300

    code update

--- bins/experiments/biggan/exp_18.sh
+++ bins/experiments/biggan/exp_18.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-export CUDA_VISIBLE_DEVICES=0,1; \
+export CUDA_VISIBLE_DEVICES=0; \
 python3 src/main.py \
 --architecture big --criterion mse --loss simple \
 --data_path resources/celeba --logdir logs/big_gan/m_s/ \
---write_period 300 --fid_period 3000 --batch_size 128 \
+--write_period 300 --fid_period 3000 --batch_size 5 \
 --img_size 256 --n_epoch 100
